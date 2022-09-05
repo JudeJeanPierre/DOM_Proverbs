d388870acc5851657a71c305c874ad3f911c0185
JudeJeanPierre <110211488+JudeJeanPierre@users.noreply.github.com>
2022-09-05 01:06:11 -0500

    DOM Provers

---
+++ main.js
@@ -0,0 +1,7 @@
+function changeBodyBg(color){
+    document.body.style.background = color;
+}
+
+function changeimg(){
+document.getElementById('provContainer').style.backgroundImage = "url(https://cdn2.geckoandfly.com/wp-content/uploads/2015/09/chinese-proverbs-quotes-saying-27.jpg)";
+}
